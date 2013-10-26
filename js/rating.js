d20db7ab83599145cac857d58e6d1cab8338a1fe
astha <sweetastha23@Asthas-MacBook.local>
2013-10-26 20:12:06 +0530

    static rating done

---
+++ js/rating.js
@@ -0,0 +1,51 @@
+    $(function() {
+
+     $('div#star').raty();
+
+     $('div#fixed').raty({
+      score: function() {
+        return $(this).attr('data-score');
+      },
+      readOnly: true
+
+    });
+
+
+     $('div#custom').raty({
+      scoreName:  'entity.score',
+      number:   10
+    });
+
+     $('div#click').raty({
+      onClick: function(score) {
+        alert('score: ' + score);
+      }
+    });
+
+     $('div#half').raty({
+      score: function() {
+        return $(this).attr('data-score');
+      },
+      showHalf: true,
+      readOnly: true
+    });
+
+     $('div#cancel').raty({
+      showCancel: true
+    });
+
+     $('div#cancel-custom').raty({
+      cancelHint: 'remove my rating!',
+      cancelPlace: 'right',
+      showCancel: true,
+      onClick: function(score) {
+        alert('score: ' + score);
+      }
+    });
+
+   });
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-17006803-1']);
+    _gaq.push(['_trackPageview']);
+    

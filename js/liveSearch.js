ea91572b478e170b8e7f7308cd0d89bcdf534581
Astha <sweetastha23@gmail.com>
2013-11-11 08:20:22 +0530

    adding search user functionality in cons.php

---
+++ js/liveSearch.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+  function search() {
+    var query_value = $('#searchFriend').val();
+    if(query_value !== ''){
+      $.ajax({
+        type: "POST",
+        url: "search.php",
+        data: { query: query_value },
+        cache: false,
+        success: function(html){
+          $("ul#results").html(html);
+        }
+      });
+    }return false;    
+  }
+
+  $("#searchFriend").live("keyup", function(e) {
+    // Set Timeout
+    clearTimeout($.data(this, 'timer'));
+
+    // Set Search String
+    var search_string = $(this).val();
+
+    // Do Search
+    if (search_string == '') {
+      $("ul#results").fadeOut();
+    }else{
+      $("ul#results").fadeIn();
+      $(this).data('timer', setTimeout(search, 100));
+    };
+  });
+});
+
+$("#onlineSearch").live("keyup", function(e) {
+    var search_string = $(this).val();
+    console.log("yo"+search_string + "yo");
+    var count = 0;
+    while($("#of" + count).html()){
+      if($("#of" + count).html().toLowerCase().match(search_string.toLowerCase()) == null){
+        $("#of" + count).hide();
+      }
+      else{
+        $("#of" + count).show();
+      }
+      count++;
+    }
+    
+
+  });
---
+++ search.php
@@ -0,0 +1,41 @@
+<?php
+
+	include("connect_sql.php");
+
+//Output HTML Formating
+$html = '';
+$html .= '<li class="result">';
+$html .= '<img src="PHOTO" width="25px" height="25px">';
+$html .= '<a href="consprofile.php?see=USERID"><font class=\"user-name\">';
+$html .= '  NAME SURNAM';
+$html .= '</a>';
+$html .= '</li>';
+
+// Get Search
+$search_string = $_POST['query'];
+// $search_string = mysql_real_escape_string($search_string);
+// echo $search_string;
+// Check Length More Than One Character
+if (strlen($search_string) >= 1 && $search_string !== ' ') {
+	$sql = "SELECT \"UserID\", \"FirstName\", \"LastName\", \"Photograph\" from \"Users\" where \"FirstName\" like '%$search_string%' or \"LastName\" like '%$search_string%' limit 5";
+	//$sql = "SELECT \"FirstName\" from \"Users\" where \"FirstName\" = 'Aaloka'";
+                  //$query = pg_query($db, $sql);
+                  //$row = pg_fetch_row($query);
+                  
+	// echo $sql;
+	$query = pg_query($db, $sql);
+	 
+ 
+  
+	
+		while($row = pg_fetch_row($query)){
+			// $output = $row[0];
+			$output = str_replace('PHOTO', $row[4], $html);
+			$output = str_replace('NAME', $row[1], $html);
+			$output = str_replace('SURNAM', $row[2], $output);
+			$output = str_replace('USERID',$row[0],$output);
+			echo "$output";
+		
+	}
+}
+?>
